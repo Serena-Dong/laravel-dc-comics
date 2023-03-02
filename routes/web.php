be204cbe6606564cae1ea61499c70f4e0840b2de
Serena-Dong <serenadong3159@icloud.com>
2023-03-02 11:49:56 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'home'])->name('home');
+
+Route::get('/characters', [PageController::class, 'characters'])->name('characters');
+
+Route::get('/comics', [PageController::class, 'comics'])->name('comics');
+
+Route::get('/movies', [PageController::class, 'movies'])->name('movies');
+
+Route::get('/tv', [PageController::class, 'tv'])->name('tv');
+
+Route::get('/games', [PageController::class, 'games'])->name('games');
+
+Route::get('/collectibles', [PageController::class, 'collectibles'])->name('collectibles');
+
+Route::get('/videos', [PageController::class, 'videos'])->name('videos');
+
+Route::get('/fans', [PageController::class, 'fans'])->name('fans');
+
+Route::get('/news', [PageController::class, 'news'])->name('news');
+
+Route::get('/shop', [PageController::class, 'shop'])->name('shop');
