93d394cc0ac2d4086aa2e05b406633fd57594974
Serena-Dong <serenadong3159@icloud.com>
2023-03-04 15:44:11 +0100

    migrate file

---
+++ database/migrations/2023_03_04_102548_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->text('thumb')->nullable();
+            $table->float('price', 6, 2);
+            $table->string('series', 50)->nullable();
+            $table->date('sale_date');
+            $table->string('type', 50);
+            $table->text('artists');
+            $table->text('writers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
