4607e4667a8b4f2637940c7218a149df74dc2e71
Serena-Dong <serenadong3159@icloud.com>
2023-03-05 18:37:23 +0100

    little fixes

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,76 +5,80 @@
 <footer>
     <div class="footer-top">
         <div class="container">
-            <div class="footer-nav">
+            <div class="row">
+                <div class="footer-nav">
 
-                <div class="footer-col">
-                    <h2>DC COMICS</h2>
-                    
-                    <ul>
-                        @foreach ($optionsOne as $option )
-
-                        <li><a href="{{ $option }}">{{ $option }}</a></li>
-
-                        @endforeach
-                    </ul>
-
-                    <h2>SHOP</h2>
-                    <ul>
-                        <li><a href="">Shop DC</a></li>
-                        <li><a href="">Shop DC Collectibles</a></li>
-                    </ul>
+                    <div class="footer-col">
+                        <h2>DC COMICS</h2>
+                        
+                        <ul>
+                            @foreach ($optionsOne as $option )
+    
+                            <li><a href="{{ $option }}">{{ $option }}</a></li>
+    
+                            @endforeach
+                        </ul>
+    
+                        <h2>SHOP</h2>
+                        <ul>
+                            <li><a href="">Shop DC</a></li>
+                            <li><a href="">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h2>DC</h2>
+                        <ul>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h2>SITES</h2>
+                        <ul>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                            <li><a href="">Characters</a></li>
+                        </ul>
+                    </div>
+    
                 </div>
-                <div class="footer-col">
-                    <h2>DC</h2>
-                    <ul>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h2>SITES</h2>
-                    <ul>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                        <li><a href="">Characters</a></li>
-                    </ul>
-                </div>
-
+                <figure>
+                    <img src=" {{Vite::asset('/resources/img/dc-logo-bg.png')}}" alt="">
+                </figure>
             </div>
-            <figure>
-                <img src=" {{Vite::asset('/resources/img/dc-logo-bg.png')}}" alt="">
-            </figure>
         </div>
     </div>
     <div class="footer-bottom">
         <div class="container">
-            <div class="footer-sign">
-                <a href="">
-                    <h2>SIGN-UP NOW!</h2>
-                </a>
-            </div>
-            <div class="footer-social">
-                <ul>
-                    <li><a href="">
-                            <h2>FOLLOW US</h2>
-                        </a></li>
-                    <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-facebook.png')}}" alt=""></a></li>
-                    <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-twitter.png')}}" alt=""></a></li>
-                    <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-youtube.png')}}" alt=""></a></li>
-                    <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-pinterest.png')}}" alt=""></a></li>
-                    <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-periscope.png')}}" alt=""></a></li>
-
-                </ul>
+            <div class="row">
+                <div class="footer-sign">
+                    <a href="">
+                        <h2>SIGN-UP NOW!</h2>
+                    </a>
+                </div>
+                <div class="footer-social">
+                    <ul>
+                        <li><a href="">
+                                <h2>FOLLOW US</h2>
+                            </a></li>
+                        <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-facebook.png')}}" alt=""></a></li>
+                        <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-twitter.png')}}" alt=""></a></li>
+                        <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-youtube.png')}}" alt=""></a></li>
+                        <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-pinterest.png')}}" alt=""></a></li>
+                        <li><a href=""><img src="{{ Vite::asset('/resources/img/footer-periscope.png')}}" alt=""></a></li>
+    
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,21 +4,23 @@
 
 <header>
     <div class="container">
-        <figure>
-            <a href="/">
-                <img src="{{ Vite::asset('/resources/img/dc-logo.png')}}" alt="">
-            </a>
-        </figure>
-        <nav>
-            @foreach ($menus as $menu)
-            <ul>
-                <li><a href="{{$menu}}">{{ $menu }}</a></li>
-            </ul>
-            @endforeach
-        </nav>
-        <div class="search">
-            <input class="searchbar" type="text" placeholder="Search">
-            <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        <div class="row">
+            <figure>
+                <a href="/">
+                    <img src="{{ Vite::asset('/resources/img/dc-logo.png')}}" alt="">
+                </a>
+            </figure>
+            <nav>
+                @foreach ($menus as $menu)
+                <ul>
+                    <li><a href="{{$menu}}">{{ $menu }}</a></li>
+                </ul>
+                @endforeach
+            </nav>
+            <div class="search">
+                <input class="searchbar" type="text" placeholder="Search">
+                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
         </div>
     </div>
 </header>
